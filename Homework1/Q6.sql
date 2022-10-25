32b747a85c74d626f4b07b5dbde9bf9882a17066
Cz-yqc <75053631+Cz-yqc@users.noreply.github.com>
2022-10-25 20:12:59 +0800

    Delete Homework1 directory

--- Homework1/Q6.sql
+++
@@ -1,8 +0,0 @@
-SELECT Id, OrderDate, PreOrderDate, round(julianday(OrderDate) - julianday(PreOrderDate),2) AS Result
-FROM (
-			SELECT Id, OrderDate, LAG(OrderDate,1,OrderDate) OVER (ORDER BY OrderDate) AS PreOrderDate
-			FROM 'Order'
-			WHERE CustomerId = 'BLONP'
-			ORDER BY OrderDate
-			LIMIT 10
-)
