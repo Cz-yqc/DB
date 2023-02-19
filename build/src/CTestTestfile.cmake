ee37bf900e9a99571ec4fdf436d7e9ee6d905bec
Cz-yqc <1204612708@qq.com>
2023-02-19 22:35:05 +0800

    first commit

---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/hgfs/Database/scudb_project2/scudb_project2/src
+# Build directory: /mnt/hgfs/Database/scudb_project2/scudb_project2/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
